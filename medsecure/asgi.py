e20cb177728affa8ecea5fd145a7bdde90b0de25
ishan shrestha <ishanstha530@gmail.com>
2024-12-19 14:06:25 +0545

    First Commit

---
+++ medsecure/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for medsecure project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medsecure.settings')
+
+application = get_asgi_application()
